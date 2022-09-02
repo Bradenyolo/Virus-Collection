6315a4f051caaa2886536b93597eeed2f1ec11fd
Bradenyolo <lebronbraden52@gmail.com>
2022-09-02 01:00:51 -0500

    Update creepy.bat

--- trash/creepy.bat
+++ trash/creepy.bat
@@ -9,8 +9,6 @@ echo EXIT THIS VIRUS IF YOU DON'T WANT YOUR
 echo ACCOUNT TO BE HACKED, ALSO YOUR COMPUTER TOO!
 pause
 cls
-echo this is a fake virus you didnt actually get hacked
-cls
 echo Accessing users...
 echo Connecting to Command and Control server...
 echo IP address found!
@@ -19,15 +17,15 @@ echo You must press any key!
 pause
 cls
 echo %username%, (: 
-echo you shouldnt've run this virus, kid...
+echo You shouldnt've run this virus, kid...
 echo cause things are bout' to get a whole lot worse...
 pause
 cls
-echo we have access to everything you own,
-echo we are going to delete this file, and leave you a note (:
-echo say the last words to you, %username%
-echo cuz we will never say anything good to you, %username%
-echo You are our next target, %username%... because you will never get your computer and accounts back... and we will do something bad to both of those things so be ready for your work to all be gone... happy hackers day!... and also we will say something to you, %username%... you know you have been hacked by archer hackers right? well you need to contribute to our team or else we will use your location for fun... (: so do it quick! and heres the link: https://ahakersopl.com/contrib/victim > %userprofile%\desktop\note.txt 
+echo We have access to everything you own,
+echo We are going to delete this file, and leave you a note (:
+echo say the last words to you, yes you %username%,
+echo because, %username% you are completely hacked and you will be kept in our so-called "victims lair"...
+echo You are our next target, %username%... because you will never get your computer and accounts back... and we will do something bad to both of those things so be ready for your work to all be gone... archer hackers are happy! (:... and also we will say something to you, %username%... you know you have been hacked by archer hackers right? well you need to contribute to our team or else we will use your location for fun... (: so do it quick! and heres the link: https://ahakersopl.com/contrib/victim > %userprofile%\desktop\note.txt 
 echo We will never forget you and we will meet you soon, %username%... so always contribute to the archer hackers team fast, but here is a quick note: %username% will be a never forgotten victim of ours... > %userprofile%\desktop\remembertokeep.txt
 copy %userprofile%\desktop\creepy.bat C:\Windows\Temp
 del %userprofile%\desktop\creepy.bat
