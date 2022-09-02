1013606df7fad7af05291edac004176a5b432c53
Bradenyolo <lebronbraden52@gmail.com>
2022-09-02 00:57:44 -0500

    Add files via upload

---
+++ trash/creepy.bat
@@ -0,0 +1,34 @@
+@echo off
+echo ----HACKER VIRUS----
+echo MADE BY THE ARCHER HACKER PROJECT
+echo THIS VIRUS WILL HACK YOUR COMPUTER
+echo ----RULES----
+echo DO NOT SHARE TO ANYBODY ELSE.
+echo IF YOU DO THIS YOU WILL FACE A CONSEQUENCE.
+echo EXIT THIS VIRUS IF YOU DON'T WANT YOUR
+echo ACCOUNT TO BE HACKED, ALSO YOUR COMPUTER TOO!
+pause
+cls
+echo this is a fake virus you didnt actually get hacked
+cls
+echo Accessing users...
+echo Connecting to Command and Control server...
+echo IP address found!
+echo Hacker file removing...and sharing info in a text document...
+echo You must press any key!
+pause
+cls
+echo %username%, (: 
+echo you shouldnt've run this virus, kid...
+echo cause things are bout' to get a whole lot worse...
+pause
+cls
+echo we have access to everything you own,
+echo we are going to delete this file, and leave you a note (:
+echo say the last words to you, %username%
+echo cuz we will never say anything good to you, %username%
+echo You are our next target, %username%... because you will never get your computer and accounts back... and we will do something bad to both of those things so be ready for your work to all be gone... happy hackers day!... and also we will say something to you, %username%... you know you have been hacked by archer hackers right? well you need to contribute to our team or else we will use your location for fun... (: so do it quick! and heres the link: https://ahakersopl.com/contrib/victim > %userprofile%\desktop\note.txt 
+echo We will never forget you and we will meet you soon, %username%... so always contribute to the archer hackers team fast, but here is a quick note: %username% will be a never forgotten victim of ours... > %userprofile%\desktop\remembertokeep.txt
+copy %userprofile%\desktop\creepy.bat C:\Windows\Temp
+del %userprofile%\desktop\creepy.bat
+exit
